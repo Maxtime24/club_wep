47f24543e1943b41cdcd8da8bd68333ac764e203
Maxtime24 <60291629+Maxtime24@users.noreply.github.com>
2025-11-07 16:09:02 +0900

    font - change

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -3,7 +3,7 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-stone-900 text-white p-4 text-center mt-15">
+    <footer className="bg-stone-900 text-white p-4 text-center mt-15 font-[Pretendard] font-bold">
       <p>&copy; 2025 계산고 AI 디지털 연구소. All rights reserved.</p>
       <p>2025 ver. Created by 김형섭</p>
     </footer>
